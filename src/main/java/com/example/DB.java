46e5e2bfb468e6e3cd7efe1f643cb280c374e312
Steve <spark89@gmail.com>
2020-05-05 21:38:24 -0400

    deleted shit

--- src/main/java/com/example/DB.java
+++ src/main/java/com/example/DB.java
@@ -1,38 +1,32 @@
 package com.example;
 
 import javax.sql.DataSource;
-import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class DB {
-
-    //private DataSource dataSource;
 
     public DB(){
 
     }
 
     public static void createTable(Connection connection, String tableName, Expense expense) throws SQLException {
-        System.out.println("@createTable");
         Statement stmt = connection.createStatement();
         String inquiry = "CREATE TABLE IF NOT EXISTS " + tableName;
         inquiry += " (" + expense.getAttributes() +")";
         stmt.executeUpdate(inquiry);
-        System.out.println("created table called " + tableName);
     }
 
     private static void insertExpense(Connection connection, String tableName, Expense expense) throws SQLException {
-        System.out.println("@insertExpense");
         Statement stmt = connection.createStatement();
         String inquiry = "INSERT INTO " + tableName + " (userid, name, cost, note, dateofpurchase, id) VALUES";
         inquiry += " (" + "'" + expense.getUserId() + "'" + ", " +
                 "'" + expense.getName() + "'" + ", " + expense.getCost() + ", " + "'" + expense.getNote() + "'" + ", "
-                + "'" + expense.getDateOfPurchase() + "'" + ", " + "'" + expense.getId() + "'" + ")";
+                + "'" + expense.getDateOfPurchase() + "'" + ", " + "'" + expense.getId() + "'" +
+                ")"; // TODO: clean this up
         System.out.println(inquiry);
         stmt.executeUpdate(inquiry);
         System.out.println("inserted expense " + expense.toString());
@@ -42,13 +36,6 @@ public class DB {
         try(Connection connection = dataSource.getConnection()) {
             createTable(connection, tableName, expense);
             insertExpense(connection, tableName, expense);
-            Statement stmt = connection.createStatement();
-//            ResultSet rs = stmt.executeQuery("SELECT * FROM " + tableName);
-//
-//            while (rs.next()) {
-//                System.out.println(rs.toString());
-//            }
-
         } catch (Exception e) {
             System.err.println("write to a table Failed " + e.getMessage());
         }
